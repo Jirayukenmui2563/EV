49b9c1c4751262ffbc6a75e64f298e712543f2b1
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-04-16 15:37:38 +0700

    Update v_main_group_sdm.php

--- application/views/consent/ev_group/v_main_group_sdm.php
+++ application/views/consent/ev_group/v_main_group_sdm.php
@@ -448,7 +448,7 @@ function Delete_data(gru_id){
 					<div class="btn-group pull-left">	
 						<button type="button" class="btn btn-inverse" data-dismiss="modal">NO</button>
 					</div>
-						<button type="button" class="btn btn-success" onClick="Delete_data(gru_id)">YES</button>
+						<button type="button" class="btn btn-success" onclick="Delete_data()">YES</button>
 				</div>
 				<!-- Modal footer -->
 			</div>
