c27809fe0c35aee8212171709243ff8221d94acf
Jirayukenmui2563 <61160053@go.buu.ac.th>
2021-04-17 13:03:06 +0700

    Update v_main_group_sdm.php

--- application/views/consent/ev_group/v_main_group_sdm.php
+++ application/views/consent/ev_group/v_main_group_sdm.php
@@ -243,7 +243,7 @@ function Delete_data(gru_id){
 														<div class="btn-group pull-left">	
 															<button type="button" class="btn btn-inverse" data-dismiss="modal">NO</button>
 														</div>
-															<button type="button" class="btn btn-success" onClick="Delete(<?echo $row->gru_id?>)">YES</button>
+															<button type="button" class="btn btn-success" onClick="Delete_data(<?echo $row->gru_id?>)">YES</button>
 													</div>
 													<!-- Modal footer -->
 												</div>
@@ -373,102 +373,9 @@ function Delete_data(gru_id){
 	</div>
 	<!-- End Modal Add-->
 
-<!-- Model Edit -->	
-	<div class="modal fade" id="Edit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header" style="background-color:gray;">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><font color="White"><b>&times;</b></font></button>
-						<h2 class="modal-title"><b><font color="white">Edit Group Data & Head Dept.</font></b></h2>		
-				</div>
-				<!-- modal header -->
-				
-				<div class="modal-body">
-					<form class="form-horizontal">
-						<div class="form-group">
-							<label for="focusedinput" class="col-sm-3 control-label">Group Name</label>
-								<div class="col-sm-6">
-									<input type="text" class="form-control" id="focusedinput" placeholder="HR AGM">
-								</div>						
-						</div>
-						<!-- Group Name -->
-						
-						<h2 style="font-family:'Courier New'"><b><font size = "4px" color="Black">Select Head Dept.</font></b></h2>
-						
-						<div class="form-group">
-							<label for="focusedinput" class="col-sm-3 control-label">Emp. ID</label>
-								<div class="col-sm-6">
-									<input type="text" class="form-control" id="focusedinput" placeholder="JS000xxx">
-								</div>
-						</div>
-						<!--Emp. ID -->
-								
-						<div class="form-group">
-							<label for="focusedinput" class="col-sm-3 control-label">Name - Surname</label>
-								<div class="col-sm-6">
-									<input disabled type="text" class="form-control" id="focusedinput" placeholder="Name Surname">
-								</div>
-						</div>
-						<!-- Name Surname -->
-					</form>
-					<!-- form-horizontal -->
-				</div>
-				<!-- modal-body -->
-							
-				<div class="modal-footer">
-					<div class="btn-group pull-left">	
-						<button type="button" class="btn btn-inverse" data-dismiss="modal">CANCEL</button>
-					</div>
-						<a href ="<?php echo base_url(); ?>/ev_group/Evs_group/select_company_sdm">
-							<button type="button" class="btn btn-success">SAVE</button>
-						</a>
-				</div>
-				<!-- modal-footer -->
-			</div>
-			<!-- modal-content -->
-		</div>
-		<!-- modal-dialog -->
-	</div>
-	<!-- End Modal Edit_add-->	
+
 	
-<!-- Modal Delete -->
-	<div class="modal fade" id="Delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header" style="background-color:gray;">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><font color="White"><b>&times;</b></font></button>	
-				</div>
-				<!-- Modal header -->
-				
-				<div class="modal-body">	
-					<div class="form-horizontal">
-						<div class="form-group" align = "center">
-							<div class="col-sm-12">
-								<label for="focusedinput" class="control-label" style="font-family:'Courier New'" align = "center"><font size = "5px">Do you want to Delete Data YES or NO ?</font></label>
-							</div>
-						</div>
-					</div>
-					<!-- form-horizontal -->
-				</div>
-				<!-- Modal body -->
-				
-				<div class="modal-footer">
-					<div class="btn-group pull-left">	
-						<button type="button" class="btn btn-inverse" data-dismiss="modal">NO</button>
-					</div>
-						
-						<button type="button" class="btn btn-success" onclick="Delete_data()">YES</button>
-					
 
-				</div>
-
-				<!-- Modal footer -->
-			</div>
-			<!-- modal-content -->
-		</div>
-		<!-- modal-dialog -->
-	</div>
-	<!-- End Modal Delete -->	
 	
 	
 	
